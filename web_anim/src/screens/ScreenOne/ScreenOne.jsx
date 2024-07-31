55a6e4debfb8bd71b3937146fb185033d4535e09
Himanshu <gghimanshu333@gmail.com>
2024-07-31 12:21:29 +0530

    screenOne 01

--- web_anim/src/screens/ScreenOne/ScreenOne.jsx
+++ web_anim/src/screens/ScreenOne/ScreenOne.jsx
@@ -1,10 +1,30 @@
 
 const ScreenOne = () => {
-  return (
-    <div className="screenone-master-container">
-      ScreenOne
-    </div>
-  )
+    return (
+        <div className="screenone-master-container">
+            <div className="header-container">
+                <div className="header-logo">FuckIT</div>
+                <div className="navbar-container">
+                    <ul className="navbar">
+                        <li>tell</li>
+                        <li>your</li>
+                        <li>celebrity</li>
+                        <li>crush</li>
+                    </ul>
+                </div>
+            </div>
+            <div className="hero-container">
+                <div className="hero-text">
+                    <h1>what do i have to do to get </h1>
+                    <h1>in bed with</h1>
+                    <div className="damon-container">
+                        <h1>my one love, </h1>
+                        <h1 className="damon-salvatore">DAMON SALVATORE</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default ScreenOne
